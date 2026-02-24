62a69633d7c80d666b75e6fa279e033730640465
rahmadeka <rahmadekapw@students.amikom.ac.id>
2026-02-24 17:49:23 +0700

    Final restore of deploy configuration and fix WelcomeScreen link

--- src/Pages/WelcomeScreen.jsx
+++ src/Pages/WelcomeScreen.jsx
@@ -301,7 +301,7 @@ const WelcomeScreen = ({ onLoadingComplete }) => {
                 data-aos-delay="1200"
               >
                 <a
-                  href="rahmad eka putra wardhani"
+                  href="https://www.linkedin.com/in/putra-wardhani-1497383a9/"
                   className="inline-flex items-center gap-2 px-4 py-2 sm:px-6 sm:py-3 rounded-full relative group hover:scale-105 transition-transform duration-300"
                   target="_blank"
                   rel="noopener noreferrer"
